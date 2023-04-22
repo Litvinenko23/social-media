3d638f51f5e7eaea1c483f23b9ed5bd737e36ce5
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-04-22 13:56:48 +0300

    Created endpoint for Post

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from social_media.models import Post
+from social_media.serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
--- user/models.py
+++ user/models.py
@@ -7,8 +7,8 @@ from social_media_api import settings
 class User(AbstractUser):
     profile_picture = models.ImageField(upload_to="profile_pictures/", blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="following", symmetrical=False, blank=True)
-    following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followers", symmetrical=False, blank=True)
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="following_users", symmetrical=False, blank=True)
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followers_users", symmetrical=False, blank=True)
 
     def __str__(self):
         return self.username
