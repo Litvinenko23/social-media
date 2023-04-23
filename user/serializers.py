c23d982e76e669fa452185619e9455f7407e8308
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-04-23 13:10:57 +0300

    Created manage user view

--- user/serializers.py
+++ user/serializers.py
@@ -14,24 +14,13 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = get_user_model()
-#         fields = ("id", "email", "password", "is_staff")
-#         read_only_fields = ("is_staff",)
-#         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
-#
-#     def create(self, validated_data):
-#         """Create a new user with encrypted password and return it"""
-#         return get_user_model().objects.create_user(**validated_data)
-#
-#     def update(self, instance, validated_data):
-#         """Update a user, set the password correctly and return it"""
-#         password = validated_data.pop("password", None)
-#         user = super().update(instance, validated_data)
-#         if password:
-#             user.set_password(password)
-#             user.save()
-#
-#         return user
+    def update(self, instance, validated_data):
+        """Update a user, set the password correctly and return it"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
