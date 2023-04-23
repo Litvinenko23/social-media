5eb05ce4161ab4ddbda75c3fcb393c52119a05f9
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-04-23 13:48:41 +0300

    Added media for posts

--- social_media/models.py
+++ social_media/models.py
@@ -1,4 +1,8 @@
+import os
+import uuid
+
 from django.db import models
+from django.utils.text import slugify
 
 from social_media_api import settings
 
@@ -10,10 +14,18 @@ class Hashtag(models.Model):
         return self.name
 
 
+def post_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+
+    filename = f"{slugify(instance.user)}-{uuid.uuid4()}.{extension}"
+
+    return os.path.join("uploads/posts/", filename)
+
+
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.ImageField(upload_to="post_images/", null=True, blank=True)
+    image = models.ImageField(upload_to=post_image_file_path, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     hashtags = models.ManyToManyField("Hashtag", related_name="posts", blank=True)
--- social_media/views.py
+++ social_media/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 
 from social_media.models import Post, Hashtag
+from social_media.permissions import IsAdminOrIfAuthenticatedReadOnly
 from social_media.serializers import PostSerializer, HashtagSerializer
 
 
@@ -9,12 +10,14 @@ class HashtagViewSet(viewsets.ModelViewSet):
     queryset = Hashtag.objects.all()
     serializer_class = HashtagSerializer
     authentication_classes = (TokenAuthentication, )
-    permission_classes = ()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.prefetch_related("hashtags")
     serializer_class = PostSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
