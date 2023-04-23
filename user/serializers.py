43882ec3b475a59f0567186b985d1a3919cd9a4b
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-04-23 14:45:12 +0300

    Added authtokenserializer

--- user/serializers.py
+++ user/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
@@ -23,4 +23,38 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={"input_type": "password"}, trim_whitespace=True
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password,
+        )
+
+        if not email:
+            raise serializers.ValidationError(
+                _("Email is required."), code="email_required"
+            )
+
+        if not password:
+            raise serializers.ValidationError(
+                _("Password is required."), code="password_required"
+            )
+
+        if not user:
+            msg = _("Unable to authenticate with provided details.")
+            raise serializers.ValidationError(msg, code="authentication")
+        attrs["user"] = user
+        return attrs
 
